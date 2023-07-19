ef8a5529bde14abf078b2f1f0251cdcd47330aa6
Bret Barden <bret.barden@gmail.com>
2023-07-18 23:52:35 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 32;
+const num4 = 8; 
+const mod = num3 / num4 ;
+
+const max = Math.max(1, 5, 20, 6, 7)
